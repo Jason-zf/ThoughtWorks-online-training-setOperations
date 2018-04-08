b7ca6e3ac99441593fa3842380d6d3fd4938adce
Jason <baofeng_zheng@163.com>
2018-04-08 10:50:26 +0800

    complete get pop even elements

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -25,7 +26,7 @@ public class CollectionOperator {
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        return Arrays.stream(array).boxed().filter(integer -> integer%2==0).collect(Collectors.toList());
     }
 
     public int popLastElment(int[] array) {
